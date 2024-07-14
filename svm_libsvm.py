087eaf24ac9b018288865fd093d3221b0358b3e8
阎志 <yanzhi2422@mails.jlu.edu.cn>
2024-07-14 20:43:05 +0800

    update

--- svm_libsvm.py
+++ svm_libsvm.py
@@ -1,11 +1,14 @@
 from libsvm.svmutil import *
+import time
 
+t0 = time.time()
 # 读取数据
 y_train, x_train = svm_read_problem('madelon.txt')
 y_test, x_test = svm_read_problem('madelon.t')
 
-# 设置参数，使用RBF核函数，自动选择gamma（相当于scale选项）
-param = '-t 2 -c 1 -b 1 -g 0'
+
+# 设置参数，使用 RBF 核函数
+param = '-t 2 -c 10 -b 0 -g 1.9794151456041003e-06'
 
 # 训练模型
 model = svm_train(y_train, x_train, param)
@@ -14,5 +17,6 @@ model = svm_train(y_train, x_train, param)
 p_labels, p_acc, p_vals = svm_predict(y_test, x_test, model)
 
 # 计算准确率
-accuracy = p_acc[0] * 100  # p_acc[0] 返回的是准确率的小数形式
+accuracy = p_acc[0]  # p_acc[0] 返回的是准确率的小数形式
 print(f'Accuracy: {accuracy}%')
+print(f'Time: {time.time() - t0:.2f}s')
--- svmbyhand.py
+++ svmbyhand.py
@@ -5,10 +5,17 @@ from scipy.sparse import csr_matrix
 import multiprocessing as mp
 import time
 import os
+from sklearn.preprocessing import StandardScaler
 # 读取数据
 X_train, y_train = load_svmlight_file('madelon.txt')
+# 标准化
+# scaler = StandardScaler()
+# X_train = scaler.fit_transform(X_train.toarray())
+# X_train = csr_matrix(X_train)
 
 X_test, y_test = load_svmlight_file('madelon.t')
+# X_test = scaler.transform(X_test.toarray())
+# X_test = csr_matrix(X_test)
 
 epsilon = 0.001
 
@@ -16,11 +23,15 @@ epsilon = 0.001
 def cal_K(i, j):
     return X_train[i].dot(X_train[j].T)[0, 0]
 '''
-n_features = X_train.shape[1]
-var_exp = X_train.data.var()  # 计算稀疏矩阵数据的方差
-gamma = 1.0 / (n_features * var_exp)
-def cal_K(i, j):
+# n_features = X_train.shape[1]
+# var_exp = X_train.data.var()  # 计算稀疏矩阵数据的方差
+
+# print('gamma:', gamma)
+def cal_K(i, j, gamma = 1.9794151456041003e-06):
     diff = X_train[i] - X_train[j]
+    return np.exp(-gamma * diff.dot(diff.T)[0, 0])
+def cal_K2(i, j, gamma = 1.9794151456041003e-06):
+    diff = X_test[i] - X_train[j]
     return np.exp(-gamma * diff.dot(diff.T)[0, 0])
 
 def compute_K_chunk(indices):
@@ -45,21 +56,43 @@ def parallel_compute_K(num_processes):
 
 
 
+def compute_K2_chunk(indices):
+    K_chunk = np.zeros((len(indices), X_train.shape[0]))
+    for idx, i in enumerate(indices):
+        for j in range(X_train.shape[0]):
+            K_chunk[idx, j] = cal_K2(i, j)
+    return K_chunk
+def parallel_compute_K2(num_processes):
+    num_samples = X_test.shape[0]
+    chunk_size = num_samples // num_processes
+    indices = [range(i * chunk_size, (i + 1) * chunk_size) for i in range(num_processes)]
+    if num_samples % num_processes != 0:
+        indices[-1] = range(indices[-1].start, num_samples)
+
+    with mp.Pool(processes=num_processes) as pool:
+        results = pool.map(compute_K2_chunk, indices)
+
+    K2 = np.vstack(results)
+    return K2
+
 def cal_g(i, alpha, b):
     gi = b
     for j in range(X_train.shape[0]):
         gi += alpha[j] * y_train[j] * K[i, j]
     return gi
 
+# KKT条件
 def KKT(i, alpha, C):
     gi = g[i]
-    if alpha[i] < epsilon:
-        return y_train[i] * gi >= 1
-    elif alpha[i] > C - epsilon:
-        return y_train[i] * gi <= 1
-    else:
-        return fabs(y_train[i] * gi - 1) < epsilon
-
+    if y_train[i] * gi >= 1 and abs(alpha[i]) <= epsilon:
+        return True
+    elif abs(y_train[i] * gi - 1) <= epsilon and 0 < alpha[i] < C:
+        return True
+    elif y_train[i] * gi <= 1 + epsilon and abs(alpha[i] - C) <= epsilon:
+        return True
+    return False
+'''
+# 外层循环，选择违反KKT条件的点
 def selectI(alpha, C):
     for i in range(X_train.shape[0]):
         if 0 < alpha[i] < C and not KKT(i, alpha, C):
@@ -68,88 +101,102 @@ def selectI(alpha, C):
         if not KKT(i, alpha, C):
             return i
     return -1
+'''
+# 内层循环，选择使|Ei - Ej|最大的点
+def selectJ(i, E):
+    maxE = 0
+    maxIndex = -1
+    for j in range(X_train.shape[0]):
+        if j == i:
+            continue
+        if abs(E[i] - E[j]) > maxE and E[j] != 0:
+            maxE = fabs(E[i] - E[j])
+            maxIndex = j
 
-def selectJ(Ei, E):
-    if Ei > 0:
-        j = np.argmin(E)
-    else:
-        j = np.argmax(E)
-    return j
+    while maxIndex == -1 or maxIndex == i:
+        maxIndex = np.random.randint(0, X_train.shape[0])
+    return maxIndex
 
 
 def SMO(C, max_iter=1000):
+    # 初始化
     alpha = np.zeros(X_train.shape[0])
     b = 0
-
     for i in range(X_train.shape[0]):
         g[i] = cal_g(i, alpha, b)
-
-    for i in range(X_train.shape[0]):
         E[i] = g[i] - y_train[i]
-
     iter = 0
+    S = []
     while iter < max_iter:
-        print('iter:', iter)
+        print('iter:', iter+1)
         # 外层循环，选择违反KKT条件的点
-        i = selectI(alpha, C)
-        if i == -1:
-            break
-        # 内层循环，选择使|Ei - Ej|最大的点
-        j = selectJ(E[i], E)
-        alpha_i_old = alpha[i]
-        alpha_j_old = alpha[j]
-        if y_train[i] != y_train[j]:
-            L = max(0, alpha[j] - alpha[i])
-            H = min(C, C + alpha[j] - alpha[i])
-        else:
-            L = max(0, alpha[j] + alpha[i] - C)
-            H = min(C, alpha[j] + alpha[i])
-        '''if L == H:
-            continue'''
-        eta = K[i, i] + K[j, j] - 2*K[i, j]
-        while eta <= 0:
-            j = np.random.randint(0, X_train.shape[0])
-            eta = K[i, i] + K[j, j] - 2*K[i, j]
+        for i in range(X_train.shape[0]):
+            if not KKT(i, alpha, C):
+                # 内层循环，选择使|Ei - Ej|最大的点
+                j = selectJ(i, E)
+                if j == -1:
+                    break
 
-        alpha[j] = alpha_j_old + y_train[j]*(E[i]-E[j])/eta
-        if alpha[j] > H:
-            alpha[j] = H
-        elif alpha[j] < L:
-            alpha[j] = L
-        alpha[i] = alpha_i_old + y_train[i]*y_train[j]*(alpha_j_old - alpha[j])
-        # 更新Ei, Ej
-        g[i] = cal_g(i, alpha, b)
-        g[j] = cal_g(j, alpha, b)
-        E[i] = g[i] - y_train[i]
-        E[j] = g[j] - y_train[j]
-        # 更新b
-        b1 = - E[i] - y_train[i]*(alpha[i] - alpha_i_old)*K[i, i] - y_train[j]*(alpha[j] - alpha_j_old)*K[j, i] + b
-        b2 = - E[j] - y_train[i]*(alpha[i] - alpha_i_old)*K[i, j] - y_train[j]*(alpha[j] - alpha_j_old)*K[j, j] + b
-        b_old = b
-        if 0 < alpha[i] < C:
-            b = b1
-        elif 0 < alpha[j] < C:
-            b = b2
-        else:
-            b = (b1 + b2)/2
-        for k in range(X_train.shape[0]):
-                E[k] = E[k] - b_old + b
-                g[k] = g[k] - b_old + b
+                alpha_i_old = alpha[i]
+                alpha_j_old = alpha[j]
+                if y_train[i] != y_train[j]:
+                    L = max(0, alpha[j] - alpha[i])
+                    H = min(C, C + alpha[j] - alpha[i])
+                else:
+                    L = max(0, alpha[j] + alpha[i] - C)
+                    H = min(C, alpha[j] + alpha[i])
+                if L == H:
+                    continue
+
+                eta = K[i, i] + K[j, j] - 2 * K[i, j]
+                while eta <= 0:
+                    continue
+
+                alpha[j] = alpha_j_old + y_train[j] * (E[i] - E[j]) / eta
+                # 裁剪
+                if alpha[j] > H:
+                    alpha[j] = H
+                elif alpha[j] < L:
+                    alpha[j] = L
+                alpha[i] = alpha_i_old + y_train[i] * y_train[j] * (alpha_j_old - alpha[j])
+
+                # 更新b
+                b_old = b
+                b1 = - E[i] - y_train[i] * (alpha[i] - alpha_i_old) * K[i, i] - y_train[j] * (alpha[j] - alpha_j_old) * \
+                     K[
+                         j, i] + b_old
+                b2 = - E[j] - y_train[i] * (alpha[i] - alpha_i_old) * K[i, j] - y_train[j] * (alpha[j] - alpha_j_old) * \
+                     K[
+                         j, j] + b_old
+                if 0 < alpha[i] < C:
+                    b = b1
+                elif 0 < alpha[j] < C:
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2
+
+                # 更新Ei, Ej
+                g[i] = cal_g(i, alpha, b)
+                g[j] = cal_g(j, alpha, b)
+                E[i] = g[i] - y_train[i]
+                E[j] = g[j] - y_train[j]
 
         iter += 1
-    return alpha, b
+    for i in range(X_train.shape[0]):
+        if 0 < alpha[i]:
+            S.append(i)
+    return alpha, b, S
 
 
 
-def predict(w, b):
+def predict(alpha, b):
     y_predict = np.zeros(X_test.shape[0])
     for i in range(X_test.shape[0]):
-        if (np.dot(w.toarray().flatten(), X_test[i].toarray().flatten()) + b) > 0:
-            y_predict[i] = 1
-        else:
-            y_predict[i] = -1
+        y_predict[i] = b
+        for j in S:
+            y_predict[i] += alpha[j] * y_train[j] * K2[i][j]
+        y_predict[i] = 1 if y_predict[i] > 0 else -1
     return y_predict
-
 
 
 def accuracy(y_test, y_predict):
@@ -172,15 +219,18 @@ if __name__ == '__main__':
         # 读取K
         K = np.loadtxt('K_rbf.txt')
 
+    if 'K2_rbf.txt' not in os.listdir():
+        K2 = parallel_compute_K2(mp.cpu_count())
+        # 保存K2到K2.txt
+        np.savetxt('K2_rbf.txt', K)
+    else:
+        # 读取K
+        K2 = np.loadtxt('K2_rbf.txt')
     E = np.zeros(X_train.shape[0])
     g = np.zeros(X_train.shape[0])
 
-    alpha, b = SMO(10, 1000)
+    alpha, b, S = SMO(10, 2000)
 
-    w = csr_matrix((X_train.shape[1], 1))
-    for i in range(X_train.shape[0]):
-        w += alpha[i] * y_train[i] * X_train[i].T
-
-    y_predict = predict(w, b)
+    y_predict = predict(alpha, b)
     print(accuracy(y_test, y_predict))
     print('time:', time.time() - t0)
